8cb0f3bda42f472b05116915cb618bbfb393a584
David.Silva <david.silva@al.resilia.com.br>
2020-06-04 13:53:38 -0300

    Template Engine 04/06

--- src/server.js
+++ src/server.js
@@ -1,4 +1,24 @@
 const express = require("express")
 const server = express()
 
+// Public settings
+server.use(express.static("public"))
+
+// Template Engie
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+// Home
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
 server.listen(3000)
