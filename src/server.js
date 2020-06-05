af2b91f51b779f541e4039ab1f0c2f3460ec05cf
David.Silva <david.silva@al.resilia.com.br>
2020-06-05 15:21:27 -0300

    db 05/06

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+// Get Data Base 
+const db = require("./database/db")
+
 // Public settings
 server.use(express.static("public"))
+
+// Req Body
+server.use(express.urlencoded( {extend: true}))
 
 // Template Engie
 const nunjucks = require("nunjucks")
@@ -18,11 +24,62 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+    // req.query
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+    // console.log(req.body)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name ,
+            address, 
+            address2,
+            state,                 
+            city, 
+            items             
+        ) VALUES (?,?,?,?,?,?,?),
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    if(search == "") {
+        return res.render("search-results.html", {total: 0 })
+    }
+
+
+    db.all(`SELECT *FROM places WHERE city LIKE = '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+        const total = rows.length
+    })
+    return res.render("search-results.html", { places: rows, total: total })
 })
 
 server.listen(3000)
